162ef8dd97b7b63b40a540fa975835a48b877c3f
denilgabani <gabanidenil@gmail.com>
2021-11-13 18:45:05 +0530

    Added GET and POST request for posts to create and get the posts

--- post/index.js
+++ post/index.js
@@ -1,11 +1,40 @@
 const express = require("express");
+const crypto = require("crypto");
 
 const app = express();
 
 // Body Parser
 app.use(express.json());
 
+// Data
+/** Format
+ * {
+ *      postId: {Id:postId, post: content of post}
+ * }
+ *
+ */
+let posts = {};
+
 // Routes
+
+app.post("/posts", (req, res) => {
+  const postId = crypto.randomBytes(4).toString("hex"); //Generate random Id
+  posts[postId] = {
+    id: postId,
+    post: req.body.content,
+  };
+
+  res.status(201).send({
+    success: true,
+  });
+});
+
+app.get("/posts", (req, res) => {
+  res.status(200).send({
+    success: true,
+    data: posts,
+  });
+});
 
 // Port
 app.listen(4001, () => console.log("Server is listening on 4001"));
